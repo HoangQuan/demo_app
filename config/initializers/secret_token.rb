53060ab13055062794addcbbf491d84a18887313
QUAN <quan@quan-K53SV.(none)>
2012-12-11 15:12:56 +0700

    Finish demo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'e113c851b27bda6b95e205f02c660910579a1ae3c89daac35e85aa02a86ecd5d31d9bb34ad633469c3ac546b0ad9a0b2cee7841bb8bf675e63647ef8c605b470'
